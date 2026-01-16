8d440f7b369ed177d4815b64e418a99ffb6bf52c
BobbyWoodard <bobbywoodard11@gmail.com>
2026-01-15 17:14:09 -0800

    Added Post functionality between front and backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,26 +1,7 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
-
-const characters = [
-  {
-    name: "Charlie",
-    job: "Janitor"
-  },
-  {
-    name: "Mac",
-    job: "Bouncer"
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress"
-  },
-  {
-    name: "Dennis",
-    job: "Bartender"
-  }
-];
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
@@ -33,6 +14,39 @@ function MyApp() {
     function updateList(person){
         setCharacters([...characters, person]);
     }
+    function fetchUsers() {
+      const promise = fetch("http://localhost:8000/users");
+      return promise;
+    }
+
+    useEffect(() => {
+      fetchUsers()
+        .then((res) => res.json())
+        .then((json) => setCharacters(json["users_list"]))
+        .catch((error) => { console.log(error); });
+    }, [] );
+
+    function postUser(person) {
+      const promise = fetch("Http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(person),
+      });
+
+      return promise;
+    }
+
+    function updateList(person) { 
+      postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+          console.log(error);
+        })
+    }
+
+
     return (
         <div className="container">
             <Table 
